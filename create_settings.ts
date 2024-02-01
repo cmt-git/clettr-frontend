a99f7eec8132e6028617b767deb2b28577f502bc
cmt-git <72115924+cmt-git@users.noreply.github.com>
2024-02-01 10:12:57 +0800

    added settings creation

---
+++ create_settings.ts
@@ -0,0 +1,21 @@
+const fs = require("fs");
+const path = require("path");
+
+const [variable, value] = process.argv.slice(2);
+
+let data = {};
+
+//@ts-ignore
+data[variable] = value;
+
+const jsonData = JSON.stringify(data, null, 2);
+const filePath = path.resolve(__dirname, "settings.json");
+
+//@ts-ignore
+fs.writeFile(filePath, jsonData, (err) => {
+  if (err) {
+    console.error("Error writing JSON file:", err);
+  } else {
+    console.log("JSON file written successfully.");
+  }
+});
