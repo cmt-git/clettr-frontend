73aa217a55262a3c7ab9bd3ef2caa34b66aefb6e
cmt-git <72115924+cmt-git@users.noreply.github.com>
2024-02-02 11:10:08 +0800

    removed create settings, changes to gitingore

--- next.config.js
+++ next.config.js
@@ -5,3 +5,18 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: {
+//     unoptimized: true,
+//   },
+//   reactStrictMode: false,
+//   trailingSlash: true,
+//   env: {
+//     ENVIRONMENT: process.env.ENVIRONMENT,
+//   },
+//   output: "export",
+// };
+
+// module.exports = nextConfig;
