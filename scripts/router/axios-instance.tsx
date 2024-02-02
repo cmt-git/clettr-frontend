4046085d0faa4d5023529dd8aa422a97b3945b50
cmt-git <72115924+cmt-git@users.noreply.github.com>
2024-02-02 12:31:26 +0800

    new out folder

--- scripts/router/axios-instance.tsx
+++ scripts/router/axios-instance.tsx
@@ -4,7 +4,7 @@ import settings from "../../settings.json";
 export const axiosInstance = axios.create({
   baseURL:
     settings.environment == "development"
-      ? `http://localhost:8878/`
-      : `http://159.223.39.105:8878/`,
+      ? `http://localhost:8878/api/`
+      : `http://159.223.39.105:8878/api/`,
   withCredentials: true,
 });
